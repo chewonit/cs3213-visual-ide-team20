96305cf86f725679ebc01886425c07d04645b598
Edward Chan <ecxc@outlook.com>
2014-10-17 17:17:14 +0800

    Minor code refactoring

--- js/Interpreter.js
+++ js/Interpreter.js
@@ -34,24 +34,22 @@ var Interpreter = (function(my) {
     };
 
     my._move = function(newX, newY) {
-        console.log("あれ～～")
+        console.log("あれ～～");
     };
 
     my._loop = function(times, container) {
         for (var i = 0; i < times; i++) {
-            container.children("ul").children("li").each(function(index, ele) {
-                execute($(this).attr('data-command-id'));
-            });
+            container.children("ul").children("li").each(execute($(this).attr('data-command-id')));
         }
     };
 
     var execute = function(procedureId) {
         var args = [Math.random() * 500, Math.random() * 500];
         if (procedureId === 5)
-            args = [Math.random() * 10, $(this)]
+            args = [Math.random() * 10, $(this)];
 
         _procedureMap[procedureId].apply(this, args);
-    }
+    };
 
     var _procedureMap = {
     	"1": my._setX,
