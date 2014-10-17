410484553493cecb15c3c6bee5359a065325b40e
Edward Chan <ecxc@outlook.com>
2014-10-17 20:06:56 +0800

    Looping works now, albeit too fast

--- js/Interpreter.js
+++ js/Interpreter.js
@@ -12,9 +12,7 @@ var Interpreter = (function(my) {
     
     my.run = function() {
     	// Loop through each procedure call in the procedure list
-        $('#list-procedures > li').each(function(index, ele) {
-            execute($(this).attr('data-command-id'));
-        }); 
+        $('#list-procedures > li').each(executeWrapper); 
     };
 
     my._setX = function(x) {
@@ -37,26 +35,49 @@ var Interpreter = (function(my) {
         console.log("あれ～～");
     };
 
+    my._changeCostume = function() {
+
+    };
+
+    my._changeBg = function() {
+
+    };
+
     my._loop = function(times, container) {
         for (var i = 0; i < times; i++) {
-            container.children("ul").children("li").each(execute($(this).attr('data-command-id')));
+            console.log(i);
+            container.children("ul").children("li").each(executeWrapper); 
         }
     };
 
-    var execute = function(procedureId) {
+    var executeWrapper = function() {
+        execute($(this));
+    };
+
+    var execute = function(procedureObj) {
+        var procedureId = procedureObj.attr('data-command-id');
+
         var args = [Math.random() * 500, Math.random() * 500];
-        if (procedureId === 5)
-            args = [Math.random() * 10, $(this)];
+        if (procedureId === "7")
+            args = [Math.random() * 10, procedureObj];
 
         _procedureMap[procedureId].apply(this, args);
     };
 
+    var getParams = function() {
+        var params = [];
+        return params;
+    };
+
     var _procedureMap = {
-    	"1": my._setX,
-    	"2": my._setY,
-    	"3": my._show,
-    	"4": my._hide,
-    	"5": my._loop
+        "0": my._setX,
+        "1": my._setY,
+        "2": my._show,
+        "3": my._hide,
+        "4": my._move,
+        "5": my._changeCostume,
+        "6": my._changeBg,
+        "7": my._loop
     };
     
     return my;
