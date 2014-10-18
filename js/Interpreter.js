4721953fddbf0811b0864d57ea90188161a79643
Edward Chan <ecxc@outlook.com>
2014-10-18 19:12:38 +0800

    Added slight delay after executing each procedure Implemented changeCostume

--- js/Interpreter.js
+++ js/Interpreter.js
@@ -3,51 +3,73 @@ var Interpreter = (function(my) {
 
     var _canvas;
     var _spriteName;
+    var _delay = 1000;
+
+    var Procedure = function(procedureId, args) {
+        this.procedureId = procedureId;
+        this.args = args;
+    };
 
     my.init = function(canvas, spriteName) {
-    	if (!canvas) throw 'Interpreter: Need canvas';
     	_canvas = canvas;
         _spriteName = spriteName;
     };
     
     my.run = function() {
-    	// Loop through each procedure call in the procedure list
-        $('#list-procedures > li').each(executeWrapper); 
+        parse($('#list-procedures > li')); 
     };
 
-    my._setX = function(x) {
+    var setX = function(x) {
         _canvas.getSprite(_spriteName).setX(x);
     };
 
-    my._setY = function(y) {
+    var setY = function(y) {
         _canvas.getSprite(_spriteName).setY(y);
     };
 
-    my._show = function() {
+    var show = function() {
         _canvas.getSprite(_spriteName).show();
     };
 
-    my._hide = function() {
+    var hide = function() {
         _canvas.getSprite(_spriteName).hide();
     };
 
-    my._move = function(newX, newY) {
+    var move = function(newX, newY) {
         console.log("あれ～～");
     };
 
-    my._changeCostume = function() {
+    var changeCostume = function(url) {
+        _canvas.getSprite(_spriteName).setImage(url);
+    };
+
+    var changeBg = function() {
 
     };
 
-    my._changeBg = function() {
-
+    // Add a slight delay after each iteration otherwise all will appear as one
+    var loop = function(numIterations, container) {
+        var i = 0;
+        var looply = function() {
+            parse(container.children("ul").children("li"));
+            if (++i < numIterations) {
+                setTimeout(looply, _delay);
+            }
+        };
+        looply();
     };
 
-    my._loop = function(times, container) {
-        for (var i = 0; i < times; i++) {
-            console.log(i);
-            container.children("ul").children("li").each(executeWrapper); 
-        }
+    // Loop through each procedure call in the procedure list
+    // Add a slight delay after each procedure otherwise all will appear as one
+    var parse = function(procedureList) {
+        var i = 0;
+        var looply = function() {
+            execute($(procedureList[i]));
+            if (++i < procedureList.length) {
+                setTimeout(looply, _delay);
+            }
+        };
+        looply();
     };
 
     var executeWrapper = function() {
@@ -56,28 +78,31 @@ var Interpreter = (function(my) {
 
     var execute = function(procedureObj) {
         var procedureId = procedureObj.attr('data-command-id');
-
-        var args = [Math.random() * 500, Math.random() * 500];
-        if (procedureId === "7")
-            args = [Math.random() * 10, procedureObj];
+        var args = getParams(procedureObj, procedureId);
 
         _procedureMap[procedureId].apply(this, args);
     };
 
-    var getParams = function() {
+    var getParams = function(procedureObj, procedureId) {
         var params = [];
+
+        params.push(procedureObj.children("input").val());
+
+        if (procedureId === "7")
+            params.push(procedureObj);
+        
         return params;
     };
 
     var _procedureMap = {
-        "0": my._setX,
-        "1": my._setY,
-        "2": my._show,
-        "3": my._hide,
-        "4": my._move,
-        "5": my._changeCostume,
-        "6": my._changeBg,
-        "7": my._loop
+        "0": setX,
+        "1": setY,
+        "2": show,
+        "3": hide,
+        "4": move,
+        "5": changeCostume,
+        "6": changeBg,
+        "7": loop
     };
     
     return my;
