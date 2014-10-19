b7f75faf1b3b8eacb2503f77f9163cd6702dba8a
Tee Ming <teemingchew@gmail.com>
2014-10-19 16:20:34 +0800

    Refactored dropbox.js

--- js/dropbox.js
+++ js/dropbox.js
@@ -1,39 +1,29 @@
 var client = new Dropbox.Client({ key: 'ac37pkslbk3prcc' });
 
 function saveProcedure() {
-    var d = new Date(); // for now
-    d.getHours(); // => 9
-    d.getMinutes(); // =>  30
-    d.getSeconds(); // => 51
+    var d = new Date();
+    d.getHours();
+    d.getMinutes();
+    d.getSeconds();
 
     var procedures = $('ul.list-procedures').html();
 
     client.writeFile('saved_data_'+d+'.txt', procedures, function (error) {
         if (error) {
-            alert('Error: ' + error);
+            // Try to complete OAuth flow.
+            alert('Error: '+error);
         } else {
             alert('File written successfully!');
         }
     });
 }
 
-        // Try to complete OAuth flow.
-        client.authenticate({ interactive: false }, function (error, client) {
-            if (error) {
-                alert('Error: ' + error);
-            }
-        });
-
-        if (client.isAuthenticated()) {
+$('#save-btn').on('click', function (e) {
+    client.authenticate(function (error, client) {
+        if (error) {
+            alert('Error: ' + error);
+        } else {
             saveProcedure();
         }
-
-        document.getElementById('saveBtn').onclick = function () {
-            client.authenticate(function (error, client) {
-                if (error) {
-                    alert('Error: ' + error);
-                } else {
-                    saveProcedure();
-                }
-            });
-        };
+    });
+});
